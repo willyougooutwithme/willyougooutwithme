d48db2d2fcdf363622e99bfae97c36bf89cc552d
willyougooutwithme <152681029+willyougooutwithme@users.noreply.github.com>
2024-12-19 10:35:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the Sosthi!";
+  question.innerHTML = "Yay, see you on 22nd";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
